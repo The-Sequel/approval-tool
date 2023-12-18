a8a74be80f94ebf5a879f1dafbf8f54ced88fbd4
SilvinvanHaestregt <stage@thesequel.nl>
2023-12-18 16:27:16 +0100

    fixed gitignore .env

--- resources/views/mail/Users/created.blade.php
+++ resources/views/mail/Users/created.blade.php
@@ -7,5 +7,8 @@
     <p>Email: {{$user->email}}</p>
     <p>Wachtwoord: {{$user->password}}</p>
 
+    {{-- Put a reset password link here --}}
+    <a href="{{route('password.request')}}">Reset password</a>
+
     <img src="{{asset('storage/'.$user->customer->logo)}}" alt="Avatar" style="width:200px; height:200px; border-radius: 50%; margin-top: 50px;">
 @endsection
