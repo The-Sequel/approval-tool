258493350c14ca74171398a28f9a6725953b1572
SilvinvanHaestregt <stage@thesequel.nl>
2024-01-15 14:27:05 +0100

    Changed the contact mail

--- resources/views/mail/Tasks/denied.blade.php
+++ resources/views/mail/Tasks/denied.blade.php
@@ -43,15 +43,3 @@
     </table>
 </body>
 </html>
-
-{{-- @extends('layouts.mail')
-
-@section('content')
-    <h2>Taak geweigerd!</h2>
-    <p>Er is een <a href="{{route('admin.tasks.show', $task->id)}}">taak</a> <span style="color: red;">geweigerd</span>!</p>
-    @if($task->project_id != null)
-        <p>Gekoppeld aan <a href="{{route('admin.projects.show', $task->project_id)}}">project</a></p>
-    @endif
-
-    <img src="{{asset('storage/'.$task->customer->logo)}}" alt="Avatar" style="width:200px; height:200px; border-radius: 50%; margin-top: 50px;">
-@endsection --}}
