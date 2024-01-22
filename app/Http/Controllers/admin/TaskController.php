cfde0825e6f82aa45f2ea6a15eb1757876fb4f80
SilvinvanHaestregt <stage@thesequel.nl>
2024-01-22 11:58:31 +0100

    small change

--- app/Http/Controllers/admin/TaskController.php
+++ app/Http/Controllers/admin/TaskController.php
@@ -155,7 +155,7 @@ class TaskController extends Controller
 
         if (preg_match($pattern, $task->description, $matches)) {
             $url = $matches[0];
-            $task->description = preg_replace($pattern, "<a href='$url'>$url</a>",$task->description);
+            $task->description = preg_replace($pattern, "<a target='_blank' href='$url'>$url</a>",$task->description);
         }
 
         return view('admin.tasks.show', compact('task', 'users', 'reasons'));
